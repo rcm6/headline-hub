4093978160be28cca1023034480926089e842002
Russell Coleman <rootofsilence@gmail.com>
2023-03-19 18:56:52 +0000

    search box and button added, modal to show filter options

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 
 function Search() {
+  const [showFilter, setShowFilter] = useState(false);
+
+  const handleFilterOpen = () => {
+    setShowFilter(true);
+  };
+
+  const handleFilterClose = () => {
+    setShowFilter(false);
+  };
+
   return (
-    <div id="Search" className="">
-      Search
+    <div className="container">
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search news..."
+          aria-label="Search news"
+          aria-describedby="search-button"
+          disabled
+        />
+        <button
+          className="btn btn-outline-secondary"
+          type="button"
+          id="search-button"
+          disabled
+        >
+          Search
+        </button>
+        <button
+          className="btn btn-outline-secondary"
+          type="button"
+          id="filter-button"
+          onClick={handleFilterOpen}
+        >
+          Filter
+        </button>
+      </div>
+      <Modal show={showFilter} onHide={handleFilterClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Filter Options</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* Replace with your filter options */}
+          <p>Filter Options</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleFilterClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
