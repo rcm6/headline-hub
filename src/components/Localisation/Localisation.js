7bcbcfeaaa937eecf6e496b45b745d76b114a27a
Russell Coleman <rootofsilence@gmail.com>
2023-03-19 11:07:28 +0000

    rewriting weather function with localisation

--- src/components/Localisation/Localisation.js
+++ src/components/Localisation/Localisation.js
@@ -7,6 +7,88 @@ import moment from "moment";
 var date = moment()
 .format('dddd, MMMM Do YYYY, h:mm a');
 
+const Weather = () => {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState('');
+
+
+
+//get the weather
+const fetchWeather = async (lat, lon) => {
+const apiKey = process.env.REACT_APP_API_KEY_OPENWEATHER;
+//const apiKey = 'd1e2d0763204896fd894698f5c6e27ee';
+//const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=50.975426715554896&lon=-3.255184197169449&appid=${apiKey}`;
+const response = await axios.get(url);
+    setWeather({
+      description: response.data.weather[0].description,
+      temp: response.data.main.temp,
+      city: response.data.name,
+      country: response.data.sys.country,
+      image: response.data.weather[0].icon,
+    });
+    setCity(response.data.name);
+  };
+
+  useEffect(() => {
+    //get user's current location
+    const getPosition = () => {
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+    };
+
+    if (navigator.geolocation) {
+      getPosition()
+        .then((position) => {
+          //If user allows location service then fetch data & send it to fetchWeather function
+          fetchWeather(position.coords.latitude, position.coords.longitude);
+        })
+        .catch((err) => {
+          //If user denies location service then send default lat/long to fetchWeather function
+          fetchWeather(42.4337516, -84.019569);
+          alert(
+            'You have disabled location services. Allow access to your current location for real-time weather.'
+          );
+        });
+    } else {
+      alert('Geolocation not available');
+    }
+  }, []);
+    //convert temperature to celcius
+    let conTemp = parseFloat(weather.temp) - 273.15;
+
+
+
+    const DisplayWeather = () => {
+      return (
+        <div>
+          <div>
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.image}.png`}
+              alt="..."
+            />
+            {conTemp.toFixed(0)} &#8451; {weather.description} for {city},{' '}
+            {weather.country} - {date}
+          </div>
+        </div>
+      );
+    };
+  
+    return (
+      <div className="mainweather">
+        <div className="weather">
+          {weather && <DisplayWeather />}
+        </div>
+      </div>
+    );
+  };
+
+  
+  export default Weather;
+
+
+
 // geolocation
 
 /*
@@ -30,63 +112,3 @@ if (navigator.geolocation) {
 */
 
 ////////////////////////////////////////////////////////////////////////
-
-
-
-//get the weather
-function Weather() {
-
-    const [weather, setWeather] = useState('');
-    const [city, setCity] = useState('');
-    //const apiKey = process.env.REACT_APP_API_KEY
-
-    const apiCall = async (e) => {
-        e.preventDefault()
-        const apiKey = process.env.REACT_APP_API_KEY_OPENWEATHER;
-        //const apiKey = 'd1e2d0763204896fd894698f5c6e27ee';
-        //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=50.975426715554896&lon=-3.255184197169449&appid=${apiKey}`;
-        const req = axios.get(url);
-        const res = await req;
-        setWeather({
-            descp: res.data.weather[0].description,
-            temp: res.data.main.temp,
-            city: res.data.name,
-            country: res.data.sys.country,
-            image: res.data.weather[0].icon,
-        })
-
-        setCity(res.data.name)
-
-    }
-
-    //convert temperature to celcius
-    let conTemp = parseFloat(weather.temp) - 273.15;
-
-    
-
-    const DisplayWeather = () => {
-
-      
-        return <div>
-
-  <div><img src="https://openweathermap.org/img/wn/01d.png" alt="..." /> {conTemp.toFixed(0)} &#8451; {weather.descp} for {city}, {weather.country} - {date}</div>
-        </div>
-    }
-    return (<>
-        <div className="mainweather">
-            <div className="weather">
-                <form onSubmit={apiCall} className="form">
-                    <button className="btn btn-light" >click to load weather</button>
-                </form>
-
-                {weather && <DisplayWeather />}
-            </div>
-        </div>
-    </>
-    )
-}
-
-
-
-export default Weather;
