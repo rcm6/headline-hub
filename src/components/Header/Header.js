9463b1e936c05166acde5e5b4b2bbd40ed73c311
Nikki1162 <117657237+Nikki1162@users.noreply.github.com>
2023-03-21 18:46:28 +0000

    Attempt 2 - footer
    Pushing changes to footer - GitHub icon and copyright text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import Navbar from "../Navbar/Navbar";
-import Weather from "../Localisation/Weather";
 
 function Header() {
   return (
     <header id="header" className="">
       <Navbar />
+      
+            
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="jumbo-title">Headline Hub</h1>
+        </div>
+      </div>
+
     </header>
   );
 }
