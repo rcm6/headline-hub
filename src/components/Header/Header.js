d99059a9505a791aae5e2a93d4c8f3614d67f925
Nikki1162 <117657237+Nikki1162@users.noreply.github.com>
2023-03-21 12:33:37 +0000

    Header
    Added Headline Hub header and styled according to wireframe

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,17 @@ import Weather from "../Localisation/Weather";
 function Header() {
   return (
     <header id="header" className="">
+      
       <Navbar />
+      
+      <Weather />
+      
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="jumbo-title">Headline Hub</h1>
+        </div>
+      </div>
+
     </header>
   );
 }
